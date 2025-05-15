94b497522a84e4a4bbabba226a5c92aa8b62c3f5
fjzeit <fjzeit@fastmail.fm>
2025-05-15 22:00:09 +1000

    tests and fixes

--- src/encoding/decode.rs
+++ src/encoding/decode.rs
@@ -26,6 +26,19 @@ impl SearchNode {
     fn add_dah(&mut self) {
         self.dah_node = Some(Box::new(SearchNode::new()));
     }
+
+    fn next(&self, c: char) -> &Option<Box<SearchNode>> {
+        match c {
+            c @ (morse::DIT | morse::DAH) => {
+                if c == morse::DIT {
+                    &self.dit_node
+                } else {
+                    &self.dah_node
+                }
+            }
+            _ => &None,
+        }
+    }
 }
 
 // Construct a dichotomic search tree to be used for morse-to-character decoding.
@@ -33,11 +46,10 @@ fn build_tree() -> SearchNode {
     let mut root = SearchNode::new();
 
     for n in morse::MORSE_TABLE.iter() {
-        if n.1.contains('/') {
-            continue;
-        };
-
         let mut node = &mut root;
+        if n.1.contains(morse::MEDIUM_GAP) || n.1.contains(morse::SHORT_GAP) {
+            continue;
+        }
         for c in n.1.chars() {
             match c {
                 morse::DIT => {
@@ -66,23 +78,46 @@ pub fn to_text(morse: &str) -> String {
     let tree = build_tree();
     let mut node = &tree;
 
-    for (i, c) in morse.chars().enumerate() {
-        if c == morse::MEDIUM_GAP || c == morse::SHORT_GAP {
-            node.value.map(|v| result.push(v));
+    for c in morse.chars() {
+        match c {
+            morse::MEDIUM_GAP => {
+                node.value.map(|v| result.push(v));
+                result.push(' ')
+            }
+            morse::SHORT_GAP => {
+                node.value.map(|v| result.push(v));
+            }
+            _ => {}
         }
 
-        if c == morse::MEDIUM_GAP {
-            result.push(' ');
-        }
-
-        node = match c {
-            morse::DIT => if let Some(n) = &node.dit_node { n } else { panic!("Invalid character at {}", i)} ,
-            morse::DAH => if let Some(n) = &node.dah_node { n } else { panic!("Invalid character at {}", i)} ,
-            _ => &tree
+        node = if let Some(next) = node.next(c).as_ref() {
+            next
+        } else {
+            &tree
         }
     }
 
     node.value.map(|v| result.push(v));
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest] // ensure the lengths in the tuple match the length of the string
+    #[case("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&'@)(:,=!.-+\"?/", "")]
+    #[case("%", "0/0")]
+    fn bounce_test(#[case] test_data: &str, #[case] expected: &str) {
+        let expected: &str = if expected.len() == 0 {
+            test_data
+        } else {
+            expected
+        };
+        let intermediate = crate::encoding::encode::to_morse(expected).unwrap();
+        let actual = to_text(&intermediate);
+        assert_eq!(expected, actual);
+    }
 }
